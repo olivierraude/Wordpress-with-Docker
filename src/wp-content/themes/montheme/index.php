adac2a2ec307c8a95cfbbfd8e6f57816c4869fa1
Olivier Raude <olivierraude@gmail.com>
2021-10-15 23:05:46 -0400

    recup version develop

--- src/wp-content/themes/montheme/index.php
+++ src/wp-content/themes/montheme/index.php
@@ -1,5 +1,35 @@
 <?php get_header() ?>
 
-    Hello World! Welcome on <?php wp_title(); ?>
+<h1>Hello World!</h1>
+<p>Welcome on <?php wp_title(); ?></p>
 
+<h2>Test mes articles</h2>
+<ul>
+    <?php if (have_posts()) : ?>
+        <?php while (have_posts()) : the_post(); ?>
+        
+        <?php 
+        /* 
+        Debug de variables globales
+
+        global $post;
+        global $wp_query;
+        var_dump($post);
+        echo '<br>';
+        echo '<br>';
+        var_dump($wp_query);
+        die(); */
+        ?>
+
+            <li>
+                <a href="<?php the_permalink() ?>">
+                    <?php the_title() ?>
+                </a>
+                 - 
+                <?php the_author() ?>
+            </li>
+
+        <?php endwhile; ?>
+    <?php endif; ?>
+</ul>
 <?php get_footer() ?>
