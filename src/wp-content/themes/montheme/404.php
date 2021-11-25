1fbfdbcd884f592f0fd8952631d4f2db6abef874
olivierraude <olivierraude@gmail.com>
2021-11-25 17:34:35 -0500

    ajout 404/front-page/test home

---
+++ src/wp-content/themes/montheme/404.php
@@ -0,0 +1,8 @@
+<?php get_header() ?>
+            
+    <h1>Oupalaille!</h1>
+    <p>
+        Cette page n'existe pas...
+    </p>
+
+<?php get_footer() ?>
--- src/wp-content/themes/montheme/single-post.php
+++ src/wp-content/themes/montheme/single-post.php
@@ -1,11 +1,16 @@
 <?php get_header() ?>
 
-    <?php if (have_posts()) : ?>
-        <?php while (have_posts()) : the_post(); ?>
-            <h1><?php the_title() ?></h1>
+    <?php /* if (have_posts()) : */ ?>
+        <?php /* while (have_posts()) : the_post(); */ ?> 
 
+        <!-- Formule simplifiée en ternaire -->
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            
+            <h1><?php the_title() ?></h1>
+            <img src="<?php the_post_thumbnail_url() ?>" alt="" style="width: 100%; height: auto">
             <?php the_content() ?>
-        <?php endwhile; ?>
-    <?php endif; ?>
+
+        <?php endwhile;
+    endif; ?>
 
 <?php get_footer() ?>
